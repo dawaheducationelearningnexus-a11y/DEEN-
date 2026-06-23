76fa56ad11a4ad4c56b7fd2d6c2f48289996739c
DEEN <dawaheducationelearningnexus@gmail.com>
2026-06-23 23:10:33 +0600

    Implement counter animation functionality
    Added a counter animation that increments to a target value.

--- js/script.js
+++ js/script.js
@@ -316,3 +316,40 @@ navMenu.classList.toggle("active");
 });
 
 }
+
+// Counter Animation
+
+const counters =
+document.querySelectorAll('.counter');
+
+counters.forEach(counter=>{
+
+const updateCounter=()=>{
+
+const target=
++counter.getAttribute('data-target');
+
+const count=
++counter.innerText;
+
+const increment=
+target/100;
+
+if(count<target){
+
+counter.innerText=
+Math.ceil(count+increment);
+
+setTimeout(updateCounter,20);
+
+}else{
+
+counter.innerText=target;
+
+}
+
+};
+
+updateCounter();
+
+});
